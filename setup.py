cabb1be6ea8938254fba4fb9701d21fbe4d48083
ReuterJakob <reuter@dena.de>
2022-07-08 11:46:47 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='integrated evaluation of h2 pathways',
+    author='Jakob Reuter',
+    license='MIT',
+)
